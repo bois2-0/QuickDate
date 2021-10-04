3c2f8fa6e69f4d5f4b8a03473d30e161164f5238
David Reynolds <dreynolds1216@gmail.com>
2021-10-04 00:09:44 -0400

    Buttons

--- Shared/HomeScreenView.swift
+++ Shared/HomeScreenView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct HomeScreenView: View {
+    @State private var showDetails = false
+    let Cuisine = ["American", "Indian", "Chinese", "Korean", "Mexican", "Italian", "Mediterranean", "Thai", "Japanese"]
+    let Movies = ["Action", "Horror", "Comedy", "Fantasy", "Indie", "Drame", "Mystery", "Romance", "Thriller"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        
+        VStack(alignment: .leading) {
+                   Button{
+                       print(Cuisine.randomElement()!)
+
+                   } label: {
+                       Text("Cuisine")
+                   }
+                    
+                
+                    Button{
+                        print(Movies.randomElement()!)
+                    } label: {
+                        Text("Movies")
+                    }
+                
+            }
+        }
 }
 
 struct HomeScreenView_Previews: PreviewProvider {
