e4ce9e1187082e58ab6f17f45d3ae0cfcd8a7f83
David Reynolds <dreynolds1216@gmail.com>
2021-10-03 23:06:46 -0400

    First Commit

---
+++ Shared/DataBase.swift
@@ -0,0 +1,20 @@
+//
+//  DataBase.swift
+//  QuickDate
+//
+//  Created by David Reynolds on 10/3/21.
+//
+
+import SwiftUI
+
+struct DataBase: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DataBase_Previews: PreviewProvider {
+    static var previews: some View {
+        DataBase()
+    }
+}
---
+++ Shared/HomeScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreenView.swift
+//  QuickDate
+//
+//  Created by David Reynolds on 10/3/21.
+//
+
+import SwiftUI
+
+struct HomeScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreenView()
+    }
+}
